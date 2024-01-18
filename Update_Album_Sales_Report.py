a2dad071b3b7e76599c2fefe6c2eccf3f4e21258
jeremyngcode <156220343+jeremyngcode@users.noreply.github.com>
2024-01-18 22:20:22 +0800

    move catalog list to separate file

--- Update_Album_Sales_Report.py
+++ Update_Album_Sales_Report.py
@@ -3,60 +3,9 @@ import pandas as pd
 from openpyxl import load_workbook
 
 from settings import *
+from catalog_release_titles import *
 from Get_Soundrop_WHT import get_wht
 # -------------------------------------------------------------------------------------------------
-
-# EDIT ACCORDINGLY - Catalog release titles
-albums_list = {
-	'album1' : 'Final Fantasy: Piano Collections',
-	'album2' : 'A Disney Collection',
-	'album3' : 'Final Fantasy: Piano Flashback',
-	'album4' : 'Chopin: Mazurkas',
-	'album5' : 'Final Fantasy: Piano Collections, Vol. 2',
-	'album6' : 'Anime Piano: Shining in the Sky',
-	'album7' : 'Final Fantasy: Piano Collections, Vol. 3',
-	'album8' : 'A Disney Collection, Vol. 2',
-	'album9' : 'Kingdom Hearts: Piano Collections',
-	'album10': 'Final Fantasy: Piano Collections, Vol. 4',
-	'album11': 'NieR Gestalt & Replicant Piano Collections',
-	'album12': 'Ghibli Piano Collection'
-}
-singles_list = {
-	'single1' : 'The Random Sketches (Nos. 5, 9 & 10)',
-	'single2' : 'Mia & Sebastian\'s Theme (From "La La Land")',
-	'single3' : 'Learn to Be Lonely (From "The Phantom of the Opera")',
-	'single4' : '13 Preludes, Op. 32: XII. Allegro in G-Sharp Minor',
-	'single5' : 'Too Good at Goodbyes',
-	'single6' : '4 Piano Pieces, Op. 119: I. Intermezzo in B Minor',
-	'single7' : '24 Preludes, Op. 28: IV. Largo in E Minor',
-	'single8' : 'Thank You, Lord',
-	'single9' : 'Lost Sandbar',
-	'single10': 'Nocturne No. 20 in C-Sharp Minor, Op. Posth.',
-	'single11': 'Final Fantasy III: Piano Opera',
-	'single12': 'Final Fantasy: 2 Town Themes',
-	'single13': 'Remember Me (From "Coco") [Arranged by Hirohashi Makiko]',
-	'single14': 'ICARO -Piano Arrangement- (From "Shadow Hearts: Covenant")',
-	'single15': 'Rydia & Edward',
-	'single16': 'Chopin: 24 Preludes, Op. 28: Nos. 6 & 7',
-	'single17': 'Penelo\'s Theme (From "Final Fantasy XII") [Piano Collections]',
-	'single18': 'Voice Of No Return',
-	'single19': 'The Tower (From "NieR:Automata") [Piano Collections]',
-	'single20': 'Schala\'s Theme (From "Chrono Trigger")',
-	'single21': 'The Fading Stories -Qingce Night- (From "Genshin Impact")'
-}
-albums_list_collabs = {
-}
-singles_list_collabs = {
-	'single_collab1': 'Song of the Ancients (From "NieR:Automata")',
-	'single_collab2': 'Merry Go Round Of Life (From "Howl\'s Moving Castle") [Erhu Cover]',
-	'single_collab3': 'Always With Me (From "Spirited Away") [Erhu Cover]',
-	'single_collab4': 'Vague Hope - Cold Rain (From "NieR:Automata")',
-	'single_collab5': 'Carrying You (From "Laputa: Castle in the Sky") [Erhu Cover]',
-	'single_collab6': 'Winding River (From "Genshin Impact") [Erhu Cover] ',
-	'single_collab7': 'Mononoke Hime (From "Princess Mononoke") [Erhu Cover]'
-}
-
-full_catalog = albums_list | singles_list | albums_list_collabs | singles_list_collabs
 
 # Load csv file into a dataframe
 soundrop_df = pd.read_csv(
@@ -77,6 +26,8 @@ print(f'AFTER-TAX INCOME: ${after_tax_income}')
 print()
 
 # Check for release titles that do not exist in full_catalog.values()
+full_catalog = albums_list | singles_list | albums_list_collabs | singles_list_collabs
+
 not_found_in_catalog = [
 	release_title for release_title in summed_revenues.index
 	if release_title not in full_catalog.values()
