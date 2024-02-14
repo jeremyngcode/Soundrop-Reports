364f43b8dacc9b8c6ba670caee75922948ee5e58
jeremyngcode <156220343+jeremyngcode@users.noreply.github.com>
2024-02-14 17:31:13 +0800

    add comment

--- settings.py
+++ settings.py
@@ -7,6 +7,8 @@ statement_period = 'YYYY-MM'
 soundrop_csv_file = pathlib.Path(
 	f"C:/path/to/my/latest_soundrop_statement/{statement_period}.csv"
 )
+
+# Script writes to this file
 xl_file = pathlib.Path(
 	"C:/path/to/my/excel_file.xlsx"
 )
